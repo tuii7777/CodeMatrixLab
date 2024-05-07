4c2020649bc4f06f73b94e5051d74560d77556da
tuii7777 <jnbbersuitpun0suy@hotmail.com>
2024-05-07 22:01:49 +0700

    hello

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function longestPalindrome(s) {
-  const map = new Map();
-  let hasOdd = false;
-  let result = 0;
-  for (const char of s) {
-    map.set(char, (map.get(char) || 0) + 1);
-  }
-  for (const count of map.values()) {
-    if (count % 2 === 0) result += count;
-    else {
-      result += count - 1;
-      hasOdd = true;
-    }
-  }
-  return hasOdd ? result + 1 : result;
-}
+const message = "Hello world!";
+const messageLength = message.length;
+console.log(messageLength); // 13
